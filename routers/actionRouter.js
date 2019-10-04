5dfa738ff8acae1aa5e00f5ce6edf830218ac798
Tyler Biswell <tsbiswell@gmail.com>
2019-10-04 11:00:21 -0600

    MVP

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -10,66 +10,61 @@ const router = express.Router();
 
 // GET /api/actions endpoint to Retrieve actions - FUNCTIONAL
 router.get('/', (req, res) => {
-    Actions.get()
-      .then(actions => res.status(200).json(actions))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({ message: 'Error retrieving the actions' });
-      });
-  });
+  Actions.get()
+    .then(actions => res.status(200).json(actions))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Error retrieving the actions' });
+    });
+});
 
 // PUT /api/actions/:id endpoint to Update an action - FUNCTIONAL
-router.put('/:id', validateActionId, validateAction, (req, res) => {
-    Actions.update(req.params.id, req.body)
-      .then(action => {
-        if (action) {
-          res.status(200).json(action);
-        } else {
-          res.status(400).json({ message: 'The action could not be found' });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({ message: 'Error updating the action' });
-      });
-  });
+router.put('/:id', validateAction, (req, res) => {
+  Actions.update(req.params.id, req.body)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(400).json({ message: 'The action could not be found' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Error updating the action' });
+    });
+});
 
-// DELETE /api/actions endpoint to Delete an action
-router.delete('/:id', validateActionId, (req, res) => {
-    Actions.remove(req.params.id)
-      .then(count => {
-        if (count > 0) {
-          res.status(200).json({ message: 'The action has been deleted' });
-        } else {
-          res.status(404).json({ message: 'The action could not be found' });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({ message: 'Error removing the action' });
-      });
-  });
+// DELETE /api/actions/:id endpoint to Delete an action
+router.delete('/:id', (req, res) => {
+  Actions.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'The action has been deleted' });
+      } else {
+        res.status(404).json({ message: 'The action could not be found' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Error removing the action' });
+    });
+});
 
 // **********************************************************************
 
-// Custom Middleware
-function validateActionId(req, res, next) {
-  next();
-}
-
 // Validate body on create new action request - FUNCTIONAL
 function validateAction(req, res, next) {
-    if (!Object.keys(req.body).length) {
-      res.status(400).json({ message: 'Missing action data!' });
-    } else if (!req.body.description) {
-      res.status(400).json({ message: 'Missing required "description" field!' });
-    } else if (!req.body.notes) {
-      res.status(400).json({ message: 'Missing required "notes" field!' });
-    } else {
-      next();
-    }
+  if (!Object.keys(req.body).length) {
+    res.status(400).json({ message: 'Missing action data!' });
+  } else if (!req.body.description) {
+    res.status(400).json({ message: 'Missing required "description" field!' });
+  } else if (!req.body.notes) {
+    res.status(400).json({ message: 'Missing required "notes" field!' });
+  } else {
+    next();
   }
-  
+}
+
 // **********************************************************************
 
 module.exports = router;
